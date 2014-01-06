6ae61081f6f1c65f7b8f281ff45db5eb991ea4d6
Cris Noble <cris@crisnoble.com>
2014-01-05 20:29:01 -0800

    moving v3 into root, pushing live

---
+++ scripts/app.js
@@ -0,0 +1,36 @@
+$('#aboutLink').on('click',function(e){
+	e.preventDefault();
+	console.log('you clicked on about');
+
+	$('#about').removeClass('hidden');
+	$('#aboutLink').addClass('active');
+
+	$('#contact').addClass('hidden');
+	$('#contactLink').removeClass('active');
+});
+
+$('#contactLink').on('click',function(e){
+	e.preventDefault();
+	console.log('you clicked on contact');
+
+	$('#about').addClass('hidden');
+	$('#aboutLink').removeClass('active');
+
+	$('#contact').removeClass('hidden');
+	$('#contactLink').addClass('active');
+
+});
+
+$('#projectsLink').on('click',function(e){
+	// e.preventDefault();
+	console.log('you clicked on projects');
+});
+
+$('.headerBoundary').on('click',function(e){
+	// e.preventDefault();
+	console.log('you clicked on the header');
+	$(this).toggleClass('moar');
+});
+
+$('#aboutLink').click();
+
