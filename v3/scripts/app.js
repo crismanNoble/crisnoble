07bf4a6ff198f513821117d18b9b02998c4126a5
Cris Noble <cris@crisnoble.com>
2014-01-20 22:20:59 -0800

    syncing v3 with main

--- v3/scripts/app.js
+++ v3/scripts/app.js
@@ -1,17 +1,19 @@
 $('#aboutLink').on('click',function(e){
 	e.preventDefault();
-	console.log('you clicked on about');
+	//console.log('you clicked on about');
 
 	$('#about').removeClass('hidden');
 	$('#aboutLink').addClass('active');
 
 	$('#contact').addClass('hidden');
 	$('#contactLink').removeClass('active');
+
+	window.location.hash = '#about';
 });
 
 $('#contactLink').on('click',function(e){
 	e.preventDefault();
-	console.log('you clicked on contact');
+	//console.log('you clicked on contact');
 
 	$('#about').addClass('hidden');
 	$('#aboutLink').removeClass('active');
@@ -19,18 +21,47 @@ $('#contactLink').on('click',function(e){
 	$('#contact').removeClass('hidden');
 	$('#contactLink').addClass('active');
 
+	window.location.hash = '#contact';
+
 });
 
 $('#projectsLink').on('click',function(e){
 	// e.preventDefault();
-	console.log('you clicked on projects');
+	//console.log('you clicked on projects');
+	window.location.hash = '#projects';
 });
 
 $('.headerBoundary').on('click',function(e){
 	// e.preventDefault();
-	console.log('you clicked on the header');
+	//console.log('you clicked on the header');
 	$(this).toggleClass('moar');
 });
 
-$('#aboutLink').click();
+if ("onhashchange" in window) {
+    window.onhashchange = locationHashChanged;
+}
+
+function locationHashChanged() {
+	var hashish = window.location.hash;
+	//console.log('hashchange occoured')
+
+    if(hashish == '#about' || hashish == '#contact' || hashish == '#projects'){
+		//console.log('it was a real page, ' +hashish)
+		$(hashish+'Link').click();	
+	}
+}
+
+document.addEventListener('DOMContentLoaded',function(){
+	console.log('dom is ready')
+	var hashish = window.location.hash;
+	//console.log('you had a hash on page load, '+hashish);
+	if(hashish == '#about' || hashish == '#contact' || hashish == '#projects'){
+		//console.log('it was a real page')
+		$(hashish+'Link').click();	
+	} else {
+		$('#aboutLink').click();	
+	}
+});
+
+
 
